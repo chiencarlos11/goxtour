e570d472e3c77b841599c5cff5e52c4fb13707c9
Carlos Chang <chiencarlos11@gmail.com>
2018-08-25 12:45:19 -0400

    Handle Executive Click

--- src/components/Executives.js
+++ src/components/Executives.js
@@ -5,23 +5,30 @@ import "../stylesheets/executive.css";
 import logo from "../static/goXtour_logo.png";
 
 export default class Executives extends Component {
+
+
+  handleClick(id){
+    console.log("Executive ID = " + id)
+  }
+
+
   render() {
     return (
       <ListGroup bsClass="list">
         <img className="logo2" src={logo} width="150" alt="" />
-        <ListGroupItem href="#link1">
+        <ListGroupItem value ="1" onClick={this.handleClick.bind(this,1)}>
           <Executive name="DAVID INGRAM" title="CHIEF EXECUTIVE OFFICER" />
         </ListGroupItem>
-        <ListGroupItem href="#link1">
+        <ListGroupItem value ="2" onClick={this.handleClick.bind(this,2)}>
           <Executive name="JASON MULLIMS" title="PRESIDENT & COO" />
         </ListGroupItem>
-        <ListGroupItem href="#link1">
+        <ListGroupItem value ="3" onClick={this.handleClick.bind(this,3)}>
           <Executive name="STEVEN POOLE" title="SVP, EFS OPERATIONS" />
         </ListGroupItem>
-        <ListGroupItem href="#link1">
-          <Executive name="JASON APPEL" title="SVP & CRO" />
+        <ListGroupItem value ="4" onClick={this.handleClick.bind(this,4)}>
+          <Executive value ="5" name="JASON APPEL" title="SVP & CRO" />
         </ListGroupItem>
-        <ListGroupItem href="#link1">
+        <ListGroupItem onClick={this.handleClick.bind(this,5)}>
           <Executive
             name="SHANE PENNELL"
             title="SVP, OPERATIONS & MERCHANDISING"

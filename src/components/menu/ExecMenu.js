e6dcb49cfbbb3be075851ffad1c2da6cd66d4411
Juan Carlos Chang <chiencarlos11@gmail.com>
2018-09-02 14:11:52 -0400

    menu real pics

--- src/components/menu/ExecMenu.js
+++ src/components/menu/ExecMenu.js
@@ -1,7 +1,13 @@
 import React, { Component } from "react";
 import "./execMenu.css";
-import logo from "./goXtour_logo.png";
-import execIcon_1 from "./images/Exec_icon_1.png";
+
+//import execIcon_0 from "./images/Exec_icon_1.png";
+import logo from "../../static/goXtour_logo.png";
+import execIcon_0 from "../../static/ExecPortraits/DavidIngram.png";
+import execIcon_1 from "../../static/ExecPortraits/JasonAppel.png";
+import execIcon_2 from "../../static/ExecPortraits/JasonMullims.png";
+import execIcon_3 from "../../static/ExecPortraits/ShanePennell.png";
+import execIcon_4 from "../../static/ExecPortraits/StevenPoole.png";
 
 const execData = require("../../data/avatars.json");
 
@@ -11,11 +17,30 @@ class ExecMenu extends Component {
     this.props.displayStore(id);
   }
 
+  getPortrait(id) {
+    switch (id) {
+      case 0:
+        return execIcon_0;
+      case 1:
+        return execIcon_1;
+      case 2:
+        return execIcon_2;
+      case 3:
+        return execIcon_3;
+      case 4:
+        return execIcon_4;
+    }
+  }
+
   render() {
     let Execs = execData.avatars.map(exec => (
-      <div className="menu-cell" key={exec.id} onClick={this.handleClick.bind(this, exec.id)}>
+      <div
+        className="menu-cell"
+        key={exec.id}
+        onClick={this.handleClick.bind(this, exec.id)}
+      >
         <div className="execImageBox">
-          <img className="execImage" src={execIcon_1} alt=""/>
+          <img className="execImage" src={this.getPortrait(exec.id)} alt="" />
         </div>
         <div className="execNameBox">
           <div className="execName">{exec.name}</div>
@@ -27,7 +52,7 @@ class ExecMenu extends Component {
     return (
       <div className="container-menu">
         <div className="menu-top">
-          <img className="image-top" src={logo} alt=""/>
+          <img className="image-top" src={logo} alt="" />
         </div>
         <div className="menu-main">{Execs}</div>
       </div>

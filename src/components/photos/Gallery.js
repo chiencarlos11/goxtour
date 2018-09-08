061aadc711c1edc45bcea843e32d89fdd8b47bc1
Juan Carlos Chang <chiencarlos11@gmail.com>
2018-09-08 17:54:50 -0400

    added infinity scroll to gallery

--- src/components/photos/Gallery.js
+++ src/components/photos/Gallery.js
@@ -1,10 +1,9 @@
 import React, { Component } from "react";
 import Modal from "react-modal";
 import InstagramEmbed from "react-instagram-embed";
-import instagramData from "../../data/instagram.json";
+import InfiniteScroll from "react-infinite-scroll-component";
+import instagram from "../../data/instagram.json";
 import "./Gallery.css";
-
-import LazyLoad from "react-lazyload";
 
 const customStyles = {
   content: {
@@ -25,15 +24,15 @@ export default class Gallery extends Component {
   constructor(props) {
     super(props);
 
-    this.filteredInstagramData = instagramData;
-
     this.state = {
       currentExecId: this.props.execId,
       currentStoreId: this.props.storeId,
       currentTag: this.props.tag,
       loaded: "false",
       modalIsOpen: false,
-      current_photo: ""
+      current_photo: "",
+      instagramData: instagram.slice(0, 19),
+      displayIndex: 19
     };
 
     this.nextSlide = this.nextSlide.bind(this);
@@ -43,6 +42,20 @@ export default class Gallery extends Component {
     this.afterOpenModal = this.afterOpenModal.bind(this);
     this.closeModal = this.closeModal.bind(this);
   }
+
+  fetchMoreData = () => {
+    let currIndex = this.state.displayIndex;
+
+    let newIndex = currIndex + 10;
+    if (newIndex >= instagram.length - 1) {
+      newIndex = instagram.length - 1;
+    }
+    this.setState({ displayIndex: newIndex });
+
+    this.setState({
+      instagramData: instagram.slice(0, newIndex)
+    });
+  };
 
   openModal(instagram_photo) {
     this.setState({ modalIsOpen: true });
@@ -67,90 +80,22 @@ export default class Gallery extends Component {
   }
 
   componentDidMount() {
-    this.filterResultsByTag(this.state.currentTag);
     this.setState({ loaded: "true" });
   }
 
-  // refreshData() {
-  //   var url =
-  //     "https://api.instagram.com/v1/users/self/media/recent/?access_token=927635119.e6873b5.fcd374c1687e423e8dd81bb2a78c4744";
-
-  //   if (this.props.access_token){
-  //     url = "https://api.instagram.com/v1/users/self/media/recent/?access_token=" + this.props.access_token;
-  //   }
-
-  //   fetch(url)
-  //     .then(response => {
-  //       return response.json();
-  //     })
-  //     .then(responseData => {
-  //       console.log(responseData);
-  //       return responseData;
-  //     })
-  //     .then(data => {
-  //       this.setState({ instagramData: data });
-  //       this.filterResultsByTag(this.state.currentTag);
-  //       this.setState({ loaded: "true" });
-  //     })
-  //     .catch(err => {
-  //       console.log("fetch error" + err);
-  //     });
-  // }
-
-  filterResultsByTag(tags) {
-    if (tags === undefined || tags.length === 0) {
-      this.filteredInstagramData = instagramData;
-    } else if (tags == "__show_all__") {
-      this.filteredInstagramData = instagramData;
-    } else {
-      var photoList = [];
-      for (var tag of tags) {
-        for (var i = 0; i < instagramData.length; i++) {
-          var element = instagramData[i];
-          if (element.tags.indexOf(tag) >= 0) {
-            photoList.push(element);
-          }
-        }
-      }
-      this.filteredInstagramData = photoList;
-    }
-  }
-
-  shouldComponentUpdate(nextProps, nextState) {
-    if (nextProps.tags === undefined || nextProps.tags.length <= 0) {
-      return true;
-    } else if (nextProps.tags === ["__show_all__"]) {
-      this.filterResultsByTag(nextProps.tags);
-      return true;
-    }
-
-    if (instagramData && nextProps.tags !== this.state.currentTags) {
-      this.filterResultsByTag(nextProps.tags);
-      return true;
-    }
-
-    return false;
-  }
-
   render() {
-    const settings = {
-      className: "center",
-      centerMode: true,
-      infinite: true,
-      centerPadding: "60px",
-      slidesToShow: 3,
-      speed: 500,
-      rows: 3,
-      slidesPerRow: 1,
-      lazyLoad: true
-    };
     document.body.style.height = "unset";
     if (this.state.loaded == "true") {
       return (
         <div className="imageList">
           <div className="hover11">
-            <LazyLoad height={200} offset={100} once>
-              {this.filteredInstagramData.map(photo => (
+            <InfiniteScroll
+              dataLength={this.state.instagramData.length}
+              next={this.fetchMoreData}
+              hasMore={true}
+              loader={<h4>Loading...</h4>}
+            >
+              {this.state.instagramData.map(photo => (
                 <img
                   className="insta_image loading"
                   key={photo.id}
@@ -186,7 +131,7 @@ export default class Gallery extends Component {
                   />
                 </Modal>
               </div>
-            </LazyLoad>
+            </InfiniteScroll>
           </div>
         </div>
       );

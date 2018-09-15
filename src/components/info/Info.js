d8059e5753fb088716eec3afa5bd6eec0401108e
Mark Reeves <markreeves.85@gmail.com>
2018-09-14 22:58:59 -0400

    Changed style of info tab
    Hotfix: mobile_gallery_wrapper overflow

--- src/components/info/Info.js
+++ src/components/info/Info.js
@@ -14,24 +14,23 @@ class Info extends Component {
       <div className="infoBox">
         <div className="infoContainer">
           <div className="infoMain fontSite">
-            <p className="fontLarger">
-              <b>
-                <u>How the Site Works</u>
-              </b>
+            <p className="fontInfoHeader">How the Site Works</p>
+            <p>
+              <span className="fontBlue">&#xb7;</span>
+              &nbsp;&nbsp;Click on a Tour Member's image on the <u>Home</u> page
+              to see where they are and where they’ve visited
             </p>
             <p>
-              &#xb7;&nbsp;&nbsp;Click on a Tour Member's image on the{" "}
-              <u>Home</u> page to see where they are and where they’ve visited
+              <span className="fontBlue">&#xb7;</span>
+              &nbsp;&nbsp;Click on <u>Gallery</u> to see all pics or videos from
+              the Tour
             </p>
             <p>
-              &#183;&nbsp;&nbsp;Click on <u>Gallery</u> to see all pics or
-              videos from the Tour
+              <span className="fontBlue">&#xb7;</span>
+              &nbsp;&nbsp;Click on <u>Travel Stats</u> to see how far each Tour
+              Member has travelled during the week
             </p>
-            <p>
-              &#183;&nbsp;&nbsp;Click on <u>Travel Stats</u> to see how far each
-              Tour Member has travelled during the week
-            </p>
-            <p className="fontRed">
+            <p className="fontBlue">
               <u>NOTE:</u> Make sure you have your creative hats on when a Tour
               Member visits: Top 3 Best Pics from the Tour will win your branch
               or store $250 for your team and $250 donated to Boys and Girls
